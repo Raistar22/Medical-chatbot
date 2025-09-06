880fe3735174ff1c625f9b3238e42769911ea072
Raistar22 <yesiamritik33@gmail.com>
2025-09-06 21:33:22 +0530

    requirements changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ritik Kumar Pathak",
+    author_email="yesiamritik33@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
